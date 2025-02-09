74762e3b8da35cba544a41e9c6735c73932a028d
SudoStar <thomas.karner1@hotmail.com>
2025-02-09 14:47:30 +0100

    modify inference function to work with OEM

--- GeoSeg/inference_huge_image.py
+++ GeoSeg/inference_huge_image.py
@@ -1,14 +1,11 @@
 import argparse
 from pathlib import Path
 import glob
-from PIL import Image
 import ttach as tta
 import cv2
 import numpy as np
 import torch
 import albumentations as albu
-from catalyst.dl import SupervisedRunner
-from skimage.morphology import remove_small_holes, remove_small_objects
 from tools.cfg import py2cfg
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
@@ -20,7 +17,7 @@ import os
 
 def seed_everything(seed):
     random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -28,67 +25,53 @@ def seed_everything(seed):
     torch.backends.cudnn.benchmark = True
 
 
-def building_to_rgb(mask):
+def label2rgb(mask):
+    """
+    mask: labels (HxW)
+    """
     h, w = mask.shape[0], mask.shape[1]
     mask_rgb = np.zeros(shape=(h, w, 3), dtype=np.uint8)
     mask_convert = mask[np.newaxis, :, :]
-    mask_rgb[np.all(mask_convert == 0, axis=0)] = [255, 255, 255]
-    mask_rgb[np.all(mask_convert == 1, axis=0)] = [0, 0, 0]
-    return mask_rgb
-
-
-def pv2rgb(mask):  # Potsdam and vaihingen
-    h, w = mask.shape[0], mask.shape[1]
-    mask_rgb = np.zeros(shape=(h, w, 3), dtype=np.uint8)
-    mask_convert = mask[np.newaxis, :, :]
-    mask_rgb[np.all(mask_convert == 3, axis=0)] = [0, 255, 0]
-    mask_rgb[np.all(mask_convert == 0, axis=0)] = [255, 255, 255]
-    mask_rgb[np.all(mask_convert == 1, axis=0)] = [255, 0, 0]
-    mask_rgb[np.all(mask_convert == 2, axis=0)] = [255, 255, 0]
-    mask_rgb[np.all(mask_convert == 4, axis=0)] = [0, 204, 255]
-    mask_rgb[np.all(mask_convert == 5, axis=0)] = [0, 0, 255]
-    return mask_rgb
-
-
-def landcoverai_to_rgb(mask):
-    w, h = mask.shape[0], mask.shape[1]
-    mask_rgb = np.zeros(shape=(w, h, 3), dtype=np.uint8)
-    mask_convert = mask[np.newaxis, :, :]
-    mask_rgb[np.all(mask_convert == 3, axis=0)] = [255, 255, 255]
-    mask_rgb[np.all(mask_convert == 0, axis=0)] = [233, 193, 133]
-    mask_rgb[np.all(mask_convert == 1, axis=0)] = [255, 0, 0]
-    mask_rgb[np.all(mask_convert == 2, axis=0)] = [0, 255, 0]
-    mask_rgb = cv2.cvtColor(mask_rgb, cv2.COLOR_RGB2BGR)
-    return mask_rgb
-
-
-def uavid2rgb(mask):
-    h, w = mask.shape[0], mask.shape[1]
-    mask_rgb = np.zeros(shape=(h, w, 3), dtype=np.uint8)
-    mask_convert = mask[np.newaxis, :, :]
-    mask_rgb[np.all(mask_convert == 0, axis=0)] = [128, 0, 0]
-    mask_rgb[np.all(mask_convert == 1, axis=0)] = [128, 64, 128]
-    mask_rgb[np.all(mask_convert == 2, axis=0)] = [0, 128, 0]
-    mask_rgb[np.all(mask_convert == 3, axis=0)] = [128, 128, 0]
-    mask_rgb[np.all(mask_convert == 4, axis=0)] = [64, 0, 128]
-    mask_rgb[np.all(mask_convert == 5, axis=0)] = [192, 0, 192]
-    mask_rgb[np.all(mask_convert == 6, axis=0)] = [64, 64, 0]
-    mask_rgb[np.all(mask_convert == 7, axis=0)] = [0, 0, 0]
-    mask_rgb = cv2.cvtColor(mask_rgb, cv2.COLOR_RGB2BGR)
+    mask_rgb[np.all(mask_convert == 0, axis=0)] = [0, 0, 0]
+    mask_rgb[np.all(mask_convert == 1, axis=0)] = [128, 0, 0]
+    mask_rgb[np.all(mask_convert == 2, axis=0)] = [0, 255, 36]
+    mask_rgb[np.all(mask_convert == 3, axis=0)] = [148, 148, 148]
+    mask_rgb[np.all(mask_convert == 4, axis=0)] = [255, 255, 255]
+    mask_rgb[np.all(mask_convert == 5, axis=0)] = [34, 97, 38]
+    mask_rgb[np.all(mask_convert == 6, axis=0)] = [222, 31, 7]
+    mask_rgb[np.all(mask_convert == 7, axis=0)] = [75, 181, 73]
+    mask_rgb[np.all(mask_convert == 8, axis=0)] = [0, 69, 255]
     return mask_rgb
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg("-i", "--image_path", type=Path, required=True, help="Path to  huge image folder")
+    arg(
+        "-i",
+        "--image_path",
+        type=Path,
+        required=True,
+        help="Path to  huge image folder",
+    )
     arg("-c", "--config_path", type=Path, required=True, help="Path to  config")
-    arg("-o", "--output_path", type=Path, help="Path to save resulting masks.", required=True)
-    arg("-t", "--tta", help="Test time augmentation.", default=None, choices=[None, "d4", "lr"])
+    arg(
+        "-o",
+        "--output_path",
+        type=Path,
+        help="Path to save resulting masks.",
+        required=True,
+    )
+    arg(
+        "-t",
+        "--tta",
+        help="Test time augmentation.",
+        default=None,
+        choices=[None, "d4", "lr"],
+    )
     arg("-ph", "--patch-height", help="height of patch size", type=int, default=512)
     arg("-pw", "--patch-width", help="width of patch size", type=int, default=512)
     arg("-b", "--batch-size", help="batch size", type=int, default=2)
-    arg("-d", "--dataset", help="dataset", default="pv", choices=["pv", "landcoverai", "uavid", "building"])
     return parser.parse_args()
 
 
@@ -101,9 +84,14 @@ def get_img_padded(image, patch_size):
     # print(oh, ow, rh, rw, height_pad, width_pad)
     h, w = oh + height_pad, ow + width_pad
 
-    pad = albu.PadIfNeeded(min_height=h, min_width=w, position='bottom_right',
-                           border_mode=0, value=[0, 0, 0])(image=image)
-    img_pad = pad['image']
+    pad = albu.PadIfNeeded(
+        min_height=h,
+        min_width=w,
+        position="bottom_right",
+        border_mode=0,
+        value=[0, 0, 0],
+    )(image=image)
+    img_pad = pad["image"]
     return img_pad, height_pad, width_pad
 
 
@@ -116,7 +104,7 @@ class InferenceDataset(Dataset):
         img = self.tile_list[index]
         img_id = index
         aug = self.transform(image=img)
-        img = aug['image']
+        img = aug["image"]
         img = torch.from_numpy(img).permute(2, 0, 1).float()
         results = dict(img_id=img_id, img=img)
         return results
@@ -135,11 +123,19 @@ def make_dataset_for_one_huge_image(img_path, patch_size):
 
     for x in range(0, output_height, patch_size[0]):
         for y in range(0, output_width, patch_size[1]):
-            image_tile = image_pad[x:x+patch_size[0], y:y+patch_size[1]]
+            image_tile = image_pad[x : x + patch_size[0], y : y + patch_size[1]]
             tile_list.append(image_tile)
 
     dataset = InferenceDataset(tile_list=tile_list)
-    return dataset, width_pad, height_pad, output_width, output_height, image_pad, img.shape
+    return (
+        dataset,
+        width_pad,
+        height_pad,
+        output_width,
+        output_height,
+        image_pad,
+        img.shape,
+    )
 
 
 def main():
@@ -147,18 +143,16 @@ def main():
     seed_everything(42)
     patch_size = (args.patch_height, args.patch_width)
     config = py2cfg(args.config_path)
-    model = Supervision_Train.load_from_checkpoint(os.path.join(config.weights_path, config.test_weights_name+'.ckpt'), config=config)
+    model = Supervision_Train.load_from_checkpoint(
+        os.path.join(config.weights_path, config.test_weights_name + ".ckpt"),
+        config=config,
+    )
 
     model.cuda()
     model.eval()
 
     if args.tta == "lr":
-        transforms = tta.Compose(
-            [
-                tta.HorizontalFlip(),
-                tta.VerticalFlip()
-            ]
-        )
+        transforms = tta.Compose([tta.HorizontalFlip(), tta.VerticalFlip()])
         model = tta.SegmentationTTAWrapper(model, transforms)
     elif args.tta == "d4":
         transforms = tta.Compose(
@@ -175,30 +169,41 @@ def main():
     img_paths = []
     if not os.path.exists(args.output_path):
         os.makedirs(args.output_path)
-    for ext in ('*.tif', '*.png', '*.jpg'):
+    for ext in ("*.tif", "*.png", "*.jpg"):
         img_paths.extend(glob.glob(os.path.join(args.image_path, ext)))
     img_paths.sort()
     # print(img_paths)
     for img_path in img_paths:
-        img_name = img_path.split('/')[-1]
+        img_name = img_path.split("/")[-1]
         # print('origin mask', original_mask.shape)
-        dataset, width_pad, height_pad, output_width, output_height, img_pad, img_shape = \
-            make_dataset_for_one_huge_image(img_path, patch_size)
+        (
+            dataset,
+            width_pad,
+            height_pad,
+            output_width,
+            output_height,
+            img_pad,
+            img_shape,
+        ) = make_dataset_for_one_huge_image(img_path, patch_size)
         # print('img_padded', img_pad.shape)
         output_mask = np.zeros(shape=(output_height, output_width), dtype=np.uint8)
         output_tiles = []
         k = 0
         with torch.no_grad():
-            dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size,
-                                    drop_last=False, shuffle=False)
+            dataloader = DataLoader(
+                dataset=dataset,
+                batch_size=args.batch_size,
+                drop_last=False,
+                shuffle=False,
+            )
             for input in tqdm(dataloader):
                 # raw_prediction NxCxHxW
-                raw_predictions = model(input['img'].cuda())
+                raw_predictions = model(input["img"].cuda())
                 # print('raw_pred shape:', raw_predictions.shape)
                 raw_predictions = nn.Softmax(dim=1)(raw_predictions)
                 # input_images['features'] NxCxHxW C=3
                 predictions = raw_predictions.argmax(dim=1)
-                image_ids = input['img_id']
+                image_ids = input["img_id"]
                 # print('prediction', predictions.shape)
                 # print(np.unique(predictions))
 
@@ -208,23 +213,15 @@ def main():
 
         for m in range(0, output_height, patch_size[0]):
             for n in range(0, output_width, patch_size[1]):
-                output_mask[m:m + patch_size[0], n:n + patch_size[1]] = output_tiles[k][0]
+                output_mask[m : m + patch_size[0], n : n + patch_size[1]] = (
+                    output_tiles[k][0]
+                )
                 # print(output_tiles[k][1])
                 k = k + 1
 
-        output_mask = output_mask[-img_shape[0]:, -img_shape[1]:]
+        output_mask = output_mask[-img_shape[0] :, -img_shape[1] :]
 
-        # print('mask', output_mask.shape)
-        if args.dataset == 'landcoverai':
-            output_mask = landcoverai_to_rgb(output_mask)
-        elif args.dataset == 'pv':
-            output_mask = pv2rgb(output_mask)
-        elif args.dataset == 'uavid':
-            output_mask = uavid2rgb(output_mask)
-        elif args.dataset == 'building':
-            output_mask = building_to_rgb(output_mask)
-        else:
-            output_mask = output_mask
+        output_mask = label2rgb(output_mask)
         # print(img_shape, output_mask.shape)
         # assert img_shape == output_mask.shape
         cv2.imwrite(os.path.join(args.output_path, img_name), output_mask)
